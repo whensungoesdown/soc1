2ac2e60914422517c6c2cef0c9adcea4285f3e84
Mingbo Zhang <whensungoes@gmail.com>
2020-10-13 11:14:18 -0400

    update vga640x480 module
    updateram.sh
    add a script to update ram.mif and vgaram.mif
    no need to compile the whole project
    
    use the copy under the same directory of this script
    
    For example, taking which ram.mif, depends on the file path in the
    ram.v. Since we need to change ram for every test, the path in
    ram.v is "ram.mif", instead of "../../soc/cpu6/ram.mif".
    
    But for vgaram.mif vgatextram.mif and chrom.mif, don't need to
    change the for every test.
    
    now the vga is in graphic mode,
    0x10000 is the first line of the screen, 0x1009f is the last spot.
    because 640 / 4 = 160
    
    then 0x10100 is the second line and so forth.
    
    0x10000 is the space of vgaram
    
    ---------------------
    
    now the ram.mif draw three blue pixel on the fifth line
    then enconter an illegal instruction
    the trap handler print a red pixel on the sixth line

--- soc/soc_top.v
+++ soc/soc_top.v
@@ -1,24 +1,24 @@
 `include "cpu6/defines.v"
 
 module soc_top (
-	    input  clk,
-	    input  reset,
-            output [2:0] vga_rgb,
-            output vga_hsync,
-            output vga_vsync
-	    );
-	
-	reg cpu_clk;
-	reg vga_clk;
-	
-	wire vgaram_en;
-	
-	always @(posedge clk)
-	begin
-		cpu_clk = ~cpu_clk;
-		vga_clk = ~vga_clk;
-	end
-	
+   input  clk,
+   input  reset,
+   output [2:0] vga_rgb,
+   output vga_hsync,
+   output vga_vsync
+   );
+   
+   reg cpu_clk;
+   reg vga_clk;
+   
+   wire vgaram_en;
+   
+   always @(posedge clk)
+      begin
+	 cpu_clk = ~cpu_clk;
+	 vga_clk = ~vga_clk;
+      end
+   
    wire [`CPU6_XLEN-1:0] pc;
    wire [`CPU6_XLEN-1:0] instr;
    wire [`CPU6_XLEN-1:0] readdata;
@@ -29,7 +29,7 @@ module soc_top (
    
    // instantiate processor and memories
    cpu6_core core(cpu_clk, !reset, pc, instr, memwrite,
-		  dataaddr, writedata, readdata);
+      dataaddr, writedata, readdata);
 
 
    // ram size 64k
@@ -41,27 +41,35 @@ module soc_top (
    //pseudo_dcache dcache(clk, memwrite, dataaddr[9:2],
    //			writedata, readdata);
 
-	
-	
-	assign vgaram_en = dataaddr[16];
-	
-	
-	// vga ram size 64k
-   wire [7:0] vga_data;
-   wire [15:0] vga_ramaddr;
-	wire [9:0] vga_x;
-	wire [9:0] vga_y;
-   wire vga_display;
-	
-	wire [15:0] vga_ramaddr_write;
-	wire [7:0] vga_data_write;
-	
-	assign vga_ramaddr_write = {1'b0, dataaddr[14:0]};
-	assign vga_data_write = writedata[7:0];
-	
-	assign vga_ramaddr = {1'b0, vga_y[8:2], vga_x[9:2]};
    
-   vga640x480 vga(vga_clk, vga_hsync, vga_vsync, vga_x, vga_y, vga_display);
+   
+   assign vgaram_en = dataaddr[16];
+   
+   
+   // vga ram size 64k
+   wire [7:0]  vga_data;
+   wire [15:0] vga_ramaddr;
+   wire [9:0]  vga_x;
+   wire [9:0]  vga_y;
+   wire vga_display;
+   
+   wire [15:0] vga_ramaddr_write;
+   wire [7:0] vga_data_write;
+   
+   assign vga_ramaddr_write = {1'b0, dataaddr[14:0]};
+   assign vga_data_write = writedata[7:0];
+   
+   assign vga_ramaddr = {1'b0, vga_y[8:2], vga_x[9:2]};
+   
+   vga640x480 vga(
+      .clk            (      vga_clk),
+      .vga_h_sync     (    vga_hsync),
+      .vga_v_sync     (    vga_vsync),
+      .inDisplayArea  (  vga_display),
+      .CounterX       (        vga_x),
+      .CounterY       (        vga_y)
+      );
+   
    vgaram ram(vga_clk, vga_data_write, vga_ramaddr, vga_ramaddr_write, vgaram_en & memwrite, vga_data);
 
    assign vga_rgb = {3{vga_display}} & vga_data[2:0];
--- soc/vga/vga640x480.v
+++ soc/vga/vga640x480.v
@@ -1,59 +1,45 @@
 module vga640x480(
-    input wire clk,           // base clock
-    output wire o_hs,           // horizontal sync
-    output wire o_vs,           // vertical sync
-    //output wire o_blanking,     // high during blanking interval
-    //output wire o_screenend,    // high for one tick at the end of screen
-    //output wire o_animate,      // high for one tick at end of active drawing
-    output wire [9:0] o_x,      // current pixel x position
-    output wire [9:0] o_y,       // current pixel y position
-	 output wire o_active       // high during active pixel drawing
-    );
+    input clk,
+    output vga_h_sync,
+    output vga_v_sync,
+    output reg inDisplayArea,
+    output reg [9:0] CounterX,
+    output reg [9:0] CounterY
+  );
+    reg vga_HS, vga_VS;
 
-    // VGA timings https://timetoexplore.net/blog/video-timings-vga-720p-1080p
-    localparam HS_STA = 10'd16;              // horizontal sync start
-    localparam HS_END = 10'd16 + 10'd96;         // horizontal sync end
-    localparam HA_STA = 10'd16 + 10'd96 + 10'd48;    // horizontal active pixel start
-    localparam VS_STA = 10'd480 + 10'd10;        // vertical sync start
-    localparam VS_END = 10'd480 + 10'd10 + 10'd2;    // vertical sync end
-    localparam VA_END = 10'd480;             // vertical active pixel end
-    localparam LINE   = 10'd800;             // complete line (pixels)
-    localparam SCREEN = 10'd525;             // complete screen (lines)
+    wire CounterXmaxed = (CounterX == 800); // 16 + 48 + 96 + 640
+    wire CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480
 
-    reg [9:0] h_count;  // line position
-    reg [9:0] v_count;  // screen position
+    always @(posedge clk)
+    if (CounterXmaxed)
+      CounterX <= 0;
+    else
+      CounterX <= CounterX + 10'h1;
 
-    // generate sync signals (active low for 640x480)
-    assign o_hs = ~((h_count >= HS_STA) & (h_count < HS_END));
-    assign o_vs = ~((v_count >= VS_STA) & (v_count < VS_END));
-
-    // keep x and y bound within the active pixels
-    assign o_x = (h_count < HA_STA) ? 10'b0 : (h_count - HA_STA);
-    assign o_y = (v_count >= VA_END) ? (VA_END - 10'b1) : (v_count);
-
-    // blanking: high within the blanking period
-    //assign o_blanking = ((h_count < HA_STA) | (v_count > VA_END - 1));
-
-    // active: high during active pixel drawing
-    assign o_active = ~((h_count < HA_STA) | (v_count > VA_END - 1)); 
-
-    // screenend: high for one tick at the end of the screen
-    //assign o_screenend = ((v_count == SCREEN - 1) & (h_count == LINE));
-
-    // animate: high for one tick at the end of the final active pixel line
-    //assign o_animate = ((v_count == VA_END - 1) & (h_count == LINE));
-
-    always @ (posedge clk)
+    always @(posedge clk)
     begin
-		if (h_count == LINE)  // end of line
-		begin
-			h_count <= 0;
-         v_count <= v_count + 10'b1;
+      if (CounterXmaxed)
+      begin
+        if(CounterYmaxed)
+          CounterY <= 0;
+        else
+          CounterY <= CounterY + 10'h1;
       end
-      else 
-         h_count <= h_count + 10'b1;
-
-         if (v_count == SCREEN)  // end of screen
-             v_count <= 0;
     end
+
+    always @(posedge clk)
+    begin
+      vga_HS <= (CounterX > (640 + 16) && (CounterX < (640 + 16 + 96)));   // active for 96 clocks
+      vga_VS <= (CounterY > (480 + 10) && (CounterY < (480 + 10 + 2)));   // active for 2 clocks
+    end
+
+    always @(posedge clk)
+    begin
+        inDisplayArea <= (CounterX < 640) && (CounterY < 480);
+    end
+
+    assign vga_h_sync = ~vga_HS;
+    assign vga_v_sync = ~vga_VS;
+
 endmodule
