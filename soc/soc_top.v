5c30c107330dcf815a669b6a36b672b6a44c4fa0
Mingbo Zhang <whensungoes@gmail.com>
2020-10-14 11:22:29 -0400

    add vga text mode
    remove 640x480 graphic mode
    
    2KB RAM  0x10000-0x107FF
    640x400 70Hz  80x25 screen  8x16 pixel per character
    128 total ASCII characters
    
    if
    640x480 60Hz 80x30 screen 8x16 pixels 128 characters
    then
    need 4KB RAM

--- soc/soc_top.v
+++ soc/soc_top.v
@@ -42,36 +42,60 @@ module soc_top (
    //			writedata, readdata);
 
    
+
+   //
+   //  text mode 80 * 25, character 8 * 16 pixels
+   //
    
    assign vgaram_en = dataaddr[16];
    
-   
-   // vga ram size 64k
-   wire [7:0]  vga_data;
-   wire [15:0] vga_ramaddr;
-   wire [9:0]  vga_x;
-   wire [9:0]  vga_y;
-   wire vga_display;
-   
-   wire [15:0] vga_ramaddr_write;
+   wire [10:0] vga_ramaddr_write;
    wire [7:0] vga_data_write;
    
-   assign vga_ramaddr_write = {1'b0, dataaddr[14:0]};
+   assign vga_ramaddr_write = dataaddr[10:0];
    assign vga_data_write = writedata[7:0];
    
-   assign vga_ramaddr = {1'b0, vga_y[8:2], vga_x[9:2]};
-   
-   vga640x480 vga(
-      .clk            (      vga_clk),
-      .vga_h_sync     (    vga_hsync),
-      .vga_v_sync     (    vga_vsync),
-      .inDisplayArea  (  vga_display),
-      .CounterX       (        vga_x),
-      .CounterY       (        vga_y)
-      );
-   
-   vgaram ram(vga_clk, vga_data_write, vga_ramaddr, vga_ramaddr_write, vgaram_en & memwrite, vga_data);
+   text80x25 textvga (
+      .clk           (vga_clk       ),
+      .vga_hsync     (vga_hsync     ),
+      .vga_vsync     (vga_vsync     ),
+      .vga_rgb       (vga_rgb       ),
 
-   assign vga_rgb = {3{vga_display}} & vga_data[2:0];
+      .write_address (vga_ramaddr_write    ),
+      .write_data    (vga_data_write       ),
+      .write_en      (vgaram_en & memwrite )
+      );
+  
+
+ 
+   // vga ram size 64k
+
+//   assign vgaram_en = dataaddr[16];
+//   wire [7:0]  vga_data;
+//   wire [15:0] vga_ramaddr;
+//   wire [9:0]  vga_x;
+//   wire [9:0]  vga_y;
+//   wire vga_display;
+//   
+//   wire [15:0] vga_ramaddr_write;
+//   wire [7:0] vga_data_write;
+//   
+//   assign vga_ramaddr_write = {1'b0, dataaddr[14:0]};
+//   assign vga_data_write = writedata[7:0];
+//   
+//   assign vga_ramaddr = {1'b0, vga_y[8:2], vga_x[9:2]};
+//   
+//   vga640x480 vga(
+//      .clk            (      vga_clk),
+//      .vga_h_sync     (    vga_hsync),
+//      .vga_v_sync     (    vga_vsync),
+//      .inDisplayArea  (  vga_display),
+//      .CounterX       (        vga_x),
+//      .CounterY       (        vga_y)
+//      );
+//   
+//   vgaram ram(vga_clk, vga_data_write, vga_ramaddr, vga_ramaddr_write, vgaram_en & memwrite, vga_data);
+//
+//   assign vga_rgb = {3{vga_display}} & vga_data[2:0];
    
 endmodule // top
