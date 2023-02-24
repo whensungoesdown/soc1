8f92d7fd0c9d9dc63345d398444d26ec17828c3b
uty <whensungoes@gmail.com>
2023-02-24 00:05:48 -0500

    change text mode 80x25 VGA setting to 640x480
    It was 640x400 before, because 80x25 text mode's resolution is
    640x200. To get the screen full, I chose to use 640x400.
    
    Some VGA2HDMI adapter does not support 640x400, but 640x480 is
    more common and supported.

--- soc/vga/hvsync.v
+++ soc/vga/hvsync.v
@@ -8,9 +8,10 @@ module hvsync(
   );
     reg vga_HS, vga_VS;
 
+    // uty: test  try 640x480
     wire CounterXmaxed = (CounterX == 800); // 16 + 48 + 96 + 640
-    //wire CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480  // 640 * 480  60 Hz
-    wire CounterYmaxed = (CounterY == 449); // 12 + 2 + 35 + 400    // 640 * 400  70 Hz
+    wire CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480  // 640 * 480  60 Hz
+    //wire CounterYmaxed = (CounterY == 449); // 12 + 2 + 35 + 400    // 640 * 400  70 Hz 
 
     always @(posedge clk)
     if (CounterXmaxed)
@@ -32,8 +33,8 @@ module hvsync(
     always @(posedge clk)
     begin
       vga_HS <= (CounterX > (640 + 16 - 1) && (CounterX < (640 + 16 + 96))); // active for 96 clocks
-      //vga_VS <= (CounterY > (480 + 10 - 1) && (CounterY < (480 + 10 + 2))); // active for 2 clocks // 640 * 480 60Hz
-      vga_VS <= (CounterY > (400 + 12 - 1) && (CounterY < (400 + 12 + 2))); // active for 2 clocks  // 640 * 400 70Hz
+      vga_VS <= (CounterY > (480 + 10 - 1) && (CounterY < (480 + 10 + 2))); // active for 2 clocks // 640 * 480 60Hz
+      //vga_VS <= (CounterY > (400 + 12 - 1) && (CounterY < (400 + 12 + 2))); // active for 2 clocks  // 640 * 400 70Hz
     end
 
     always @(posedge clk)
